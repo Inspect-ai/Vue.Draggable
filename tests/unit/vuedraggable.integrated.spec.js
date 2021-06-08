8c6e137585acc7268f417f23044f67f8804196cb
Richard Abrich <richard.abrich@gmail.com>
2021-06-07 20:05:16 -0400

    Revert "Merge branch 'master' of https://github.com/SortableJS/Vue.Draggable into multi-drag"
    This reverts commit f3787fc695ea42d2eb58080e95b99b6d5f40842b, reversing
    changes made to bf160654746520bbcf55ab3166ec430a9f0cb1a1.

--- tests/unit/vuedraggable.integrated.spec.js
+++ tests/unit/vuedraggable.integrated.spec.js
@@ -1,17 +1,17 @@
 import { mount } from "@vue/test-utils";
 import Sortable from "sortablejs";
-jest.genMockFromModule("sortablejs");
-jest.mock("sortablejs");
+jest.genMockFromModule('sortablejs');
+jest.mock('sortablejs');
 const SortableFake = {
   destroy: jest.fn(),
-  option: jest.fn(),
+  option: jest.fn()
 };
 Sortable.mockImplementation(() => SortableFake);
 
 import Vue from "vue";
-import DraggableWithList from "./helper/DraggableWithList";
-import DraggableWithModel from "./helper/DraggableWithList";
-import DraggableWithTransition from "./helper/DraggableWithTransition";
+import DraggableWithList from "./helper/DraggableWithList"
+import DraggableWithModel from "./helper/DraggableWithList"
+import DraggableWithTransition from "./helper/DraggableWithTransition"
 
 import draggable from "@/vuedraggable";
 
@@ -24,18 +24,13 @@ function getEvent(name) {
 }
 
 const expectedArray = [0, 1, 3, 4, 5, 6, 7, 2, 8, 9];
-const expectedDomWithWrapper = (wrapper) =>
-  `<${wrapper}>${expectedArray
-    .map((nu) => `<div>${nu}</div>`)
-    .join("")}</${wrapper}>`;
+const expectedDomWithWrapper = wrapper => `<${wrapper}>${expectedArray.map(nu => `<div>${nu}</div>`).join('')}</${wrapper}>`;
 
-const expectedDomNoTransition = expectedDomWithWrapper("span");
-const expectedDomTransition = `<div>${expectedDomWithWrapper(
-  "transition-group-stub"
-)}</div>`;
+const expectedDomNoTransition = expectedDomWithWrapper('span');
+const expectedDomTransition = `<div>${expectedDomWithWrapper('transition-group-stub')}</div>`;
 
 function normalizeHTML(wrapper) {
-  return wrapper.html().replace(/(\r\n\t|\n|\r\t| )/gm, "");
+  return wrapper.html().replace(/(\r\n\t|\n|\r\t| )/gm,"");
 }
 
 function expectHTML(wrapper, expected) {
@@ -46,52 +41,50 @@ function expectHTML(wrapper, expected) {
 describe.each([
   [DraggableWithList, "draggable with list", expectedDomNoTransition, "span"],
   [DraggableWithModel, "draggable with model", expectedDomNoTransition, "span"],
-  [
-    DraggableWithTransition,
-    "draggable with transition",
-    expectedDomTransition,
-    "transition-group-stub",
-  ],
-])(
-  "should update list and DOM with component: %s %s",
-  (component, _, expectedDom, expectWrapper) => {
-    describe("when handling sort", () => {
-      beforeEach(async () => {
-        jest.resetAllMocks();
-        wrapper = mount(component);
-        vm = wrapper.vm;
-        element = wrapper.find(expectWrapper).element;
+  [DraggableWithTransition, "draggable with transition", expectedDomTransition, "transition-group-stub"]
+])
+  (
+    "should update list and DOM with component: %s %s",
+    (component, _, expectedDom, expectWrapper) => {
 
-        const item = element.children[2];
-        const startEvt = { item };
-        getEvent("onStart")(startEvt);
-        await Vue.nextTick();
+      describe("when handling sort", () => {
 
-        const firstDraggable = element.children[1];
-        element.removeChild(item);
-        element.insertBefore(item, firstDraggable);
-        getEvent("onUpdate")({
-          item,
-          oldIndex: 2,
-          newIndex: 7,
-          from: element,
+        beforeEach(async () => {
+          jest.resetAllMocks();
+          wrapper = mount(component);
+          vm = wrapper.vm;
+          element = wrapper.find(expectWrapper).element;
+
+          const item = element.children[2];
+          const startEvt = { item };
+          getEvent("onStart")(startEvt);
+          await Vue.nextTick();
+
+          const firstDraggable = element.children[1];
+          element.removeChild(item);
+          element.insertBefore(item, firstDraggable);
+          getEvent("onUpdate")({
+            item,
+            oldIndex: 2,
+            newIndex: 7,
+            from: element
+          });
+          await Vue.nextTick();
         });
-        await Vue.nextTick();
-      });
 
-      it("sends a change event", async () => {
-        const draggableWrapper = wrapper.findComponent(draggable);
-        const expectedEvt = { moved: { element: 2, oldIndex: 2, newIndex: 7 } };
-        expect(draggableWrapper.emitted().change).toEqual([[expectedEvt]]);
-      });
+        it("sends a change event", async () => {
+          const draggableWrapper = wrapper.find(draggable);
+          const expectedEvt = { moved: { element: 2, oldIndex: 2, newIndex: 7 } };
+          expect(draggableWrapper.emitted().change).toEqual([[expectedEvt]]);
+        });
 
-      it("update list", async () => {
-        expect(vm.array).toEqual(expectedArray);
-      });
+        it("update list", async () => {
+          expect(vm.array).toEqual(expectedArray);
+        });
 
-      it("updates DOM", async () => {
-        expectHTML(wrapper, expectedDom);
+        it("updates DOM", async () => {
+          expectHTML(wrapper, expectedDom);
+        });
       });
-    });
-  }
-);
+    }
+  )
